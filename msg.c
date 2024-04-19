0a6e30254a66fa8a7761ae0baee09e5869c43be0
k2matu <kstt90@gmail.com>
2024-04-19 12:04:54 +0300

    working fully. clean up and check leaks

--- msg.c
+++ msg.c
@@ -6,7 +6,7 @@
 /*   By: kmatjuhi <kmatjuhi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 13:14:28 by kmatjuhi          #+#    #+#             */
-/*   Updated: 2024/04/19 11:42:14 by kmatjuhi         ###   ########.fr       */
+/*   Updated: 2024/04/19 12:02:38 by kmatjuhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,31 @@
 
 int	print_msg(t_philo *philo, const char *msg)
 {
-	printf("inside msg for %d\n", philo->x);
+	// printf("inside msg for %d\n", philo->x);
 	pthread_mutex_lock(philo->lock);
-	printf("inside msg for locked%d\n", philo->x);
+	// printf("inside msg for locked%d\n", philo->x);
+	if ((time_in_ms() - philo->time_last_meal) > philo->die)
+	{
+		if (philo->times_eat == 0 || philo->d_flag[0] != -1)
+		{
+			printf("somebody already died, inside %d\n", philo->x);
+			pthread_mutex_unlock(philo->lock);
+			return (0);
+		}
+		printf("1. %ld %d died\n", time_in_ms(), philo->x);
+		philo->d_flag[0] = philo->x;
+		pthread_mutex_unlock(philo->lock);
+		// printf("unlocking and exiting for %d\n", philo->x);
+		return (0);
+	}
 	if (philo->times_eat == 0 || philo->d_flag[0] != -1)
 	{
 		pthread_mutex_unlock(philo->lock);
-		printf("inside msg for unlocked 1 %d\n", philo->x);
+		// printf("inside msg for unlocked 1 %d\n", philo->x);
 		return (0);
 	}
 	printf("%ld %d %s\n", time_in_ms(), philo->x, msg);
 	pthread_mutex_unlock(philo->lock);
-	printf("inside msg for unlocked 2 %d\n", philo->x);
+	// printf("inside msg for unlocked 2 %d\n", philo->x);
 	return (1);
 }
--- routine.c
+++ routine.c
@@ -6,7 +6,7 @@
 /*   By: kmatjuhi <kmatjuhi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 14:42:29 by kmatjuhi          #+#    #+#             */
-/*   Updated: 2024/04/19 11:46:58 by kmatjuhi         ###   ########.fr       */
+/*   Updated: 2024/04/19 12:03:52 by kmatjuhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,38 @@
 
 int	ft_eat(t_philo *philo)
 {
-	printf("inside eat function, forks not locked %d\n", philo->x);
+	// printf("inside eat function, forks not locked %d\n", philo->x);
 	if (*(philo->d_flag) != -1)
 	{
-		printf("exiting for %d\n",philo->x);
+		// printf("exiting for %d\n",philo->x);
 		return (0);
 	}
 	pthread_mutex_lock(philo->l_fork);
 	pthread_mutex_lock(philo->r_fork);
 	printf("inside fork locks %d\n", philo->x);
-	if ((time_in_ms() - philo->time_last_meal) > philo->die)
-	{
-		pthread_mutex_lock(philo->lock);
-		if (philo->times_eat == 0 || philo->d_flag[0] != -1)
-		{
-			printf("somebody already died\n");
-			pthread_mutex_unlock(philo->l_fork);
-			pthread_mutex_unlock(philo->r_fork);
-			pthread_mutex_unlock(philo->lock);
-			return (0);
-		}
-		printf("%ld %d died\n", time_in_ms(), philo->x);
-		philo->d_flag[0] = philo->x;
-		pthread_mutex_unlock(philo->l_fork);
-		pthread_mutex_unlock(philo->r_fork);
-		pthread_mutex_unlock(philo->lock);
-		printf("unlocking and exiting for %d\n", philo->x);
-		return (0);
-	}
+	// if ((time_in_ms() - philo->time_last_meal) > philo->die)
+	// {
+	// 	pthread_mutex_lock(philo->lock);
+	// 	if (philo->times_eat == 0 || philo->d_flag[0] != -1)
+	// 	{
+	// 		// printf("somebody already died, inside %d\n", philo->x);
+	// 		pthread_mutex_unlock(philo->l_fork);
+	// 		pthread_mutex_unlock(philo->r_fork);
+	// 		pthread_mutex_unlock(philo->lock);
+	// 		return (0);
+	// 	}
+	// 	printf("2. %ld %d died\n", time_in_ms(), philo->x);
+	// 	philo->d_flag[0] = philo->x;
+	// 	pthread_mutex_unlock(philo->l_fork);
+	// 	pthread_mutex_unlock(philo->r_fork);
+	// 	pthread_mutex_unlock(philo->lock);
+	// 	// printf("unlocking and exiting for %d\n", philo->x);
+	// 	return (0);
+	// }
 	if (print_msg(philo, "has taken a L fork") == 0)
 	{
 		pthread_mutex_unlock(philo->l_fork);
-		pthread_mutex_lock(philo->r_fork);
+		pthread_mutex_unlock(philo->r_fork);
 		return (0);
 	}
 	if (print_msg(philo, "has taken a R fork") == 0)
